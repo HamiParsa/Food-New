2d6cb4cf50391a15041adc96ca3906ce1eceb2f6
Hami <parsa.developer001@gmail.com>
2025-11-01 16:12:58 +0330

    change website

--- app/page.tsx
+++ app/page.tsx
@@ -656,11 +656,6 @@ export default function Home() {
               filteredFoods.map((f) => <FoodCard key={f.name} food={f} />)
             )}
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
-            {filteredFoods.map((f) => (
-              <FoodCard key={f.name} food={f} />
-            ))}
-          </div>
         </div>
       </section>
 
